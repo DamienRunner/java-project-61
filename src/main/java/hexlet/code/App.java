db846eedfb020422d01968e1b312bbd2d7f56583
Denis Demianov <apchjkheee@gmail.com>
2024-10-23 17:38:09 +0300

    lesson 9

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -5,10 +5,6 @@ public class App {
     private static final Engine ENGINE = new Engine();
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
-        String name = Cli.introduceYourself();
-        System.out.println("Hello, " + name);
         ENGINE.startPlayground();
     }
 }
