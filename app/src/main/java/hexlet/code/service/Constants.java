c630e90a8e841c8f9f11ec745ea865dc7d60d548
Denis Demianov <apchjkheee@gmail.com>
2024-11-03 17:36:28 +0300

    games code refactor

--- app/src/main/java/hexlet/code/service/Constants.java
+++ app/src/main/java/hexlet/code/service/Constants.java
@@ -1,9 +1,13 @@
 package hexlet.code.service;
 
-public interface Constants {
-     int MIN_RANDOM = -100;
-     int MAX_RANDOM = 100;
-     int MAX_ADDITIONAL_LENGTH = 6;
-     int MIN_LENGTH = 5;
-     int MAX_LENGTH = 10;
+public class Constants {
+     public static final int MIN_RANDOM = -100;
+     public static final int MAX_RANDOM = 100;
+     public static final int MAX_ADDITIONAL_LENGTH = 6;
+     public static final int MIN_LENGTH = 5;
+     public static final int MAX_LENGTH = 10;
+
+     private Constants() {
+          throw new UnsupportedOperationException("This is a constants class and cannot be instantiated");
+     }
 }
